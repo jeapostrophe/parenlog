c8df97e70ecbf107b83cfa0700e06398346cca5d
Jay McCarthy <jay.mccarthy@gmail.com>
2017-11-02 16:14:54 -0400

    squash

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang info
-(define collection 'multi)
+(define collection "parenlog")
 (define deps '("base"))
 (define build-deps '("eli-tester" "racket-doc" "scribble-lib"))
+
+(define scribblings '(["parenlog.scrbl" (multi-page) (language)]))
